1b0ff1862857baa3a7cb4048948041e9e4d8ac89
Lex Wilkins and Roberta Drayer <lex@>
2019-03-18 20:21:02 -0700

    create jquery to determine what movie, time, and age was selected for the ticket

--- js/scripts.js
+++ js/scripts.js
@@ -10,10 +10,25 @@ function Ticket() {
 }
 
 Ticket.prototype.calc = function() {
-
   var message = `You've selected a ${this.age} ticket to see ${this.movie} at ${this.time}`
   return message;
 }
+
+// Ticket.protoype.blankCheck = function() {
+//   if (this.movie === 'blank') {
+//
+//   }
+// }
+
+// Ticket.protoype.convert() {
+//   if (this.movie === 'tit') {
+//     this.movie = 'Titanic';
+//   } else if (this.movie === 'bttf') {
+//     this.movie = 'Back to the Future';
+//   } else {
+//
+//   }
+// }
 
 
 $(document).ready(function(){
@@ -24,10 +39,10 @@ $(document).ready(function(){
     var newTicket = new Ticket();
 
     newTicket.age = $('#ageinput').val();
-    newTicket.movie = $('#movieinput').val();
-    newTicket.time = $('input:radio[name=time]:checked').val();
+    newTicket.movie = $('#movieinput option:selected').text();
+    newTicket.time = $('input:radio[name=time]:checked').parent().text();
 
-    console.log(newTicket);
+    // console.log(newTicket);
 
     var resultMessage = newTicket.calc();
 
